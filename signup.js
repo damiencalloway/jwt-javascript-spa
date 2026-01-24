ff34ba0e77e12b1d40378f6b0480d55f5f48a147
Mark Aufdencamp <markaufdencamp@Kimberlys-Mac-mini.local>
2026-01-24 16:15:49 -0500

    Added Signup js. Renamed signup form to include -form.

---
+++ signup.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+const signupForm = document.getElementById("signup-form");
+
+signupForm.addEventListener('submit', async (e) => {
+	e.preventDefault();
+	console.log("signupForm submission");
+});
